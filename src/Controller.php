d3a22178e54b6c89a87a7849a8eebe0f2beaeb94
Kavirubc <hapuarachchikaviru@gmail.com>
2024-11-30 10:50:18 +0530

    first commit

---
+++ src/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core;
+
+/**
+ * Parent Controller class of all Controllers.
+ */
+class Controller
+{
+    //
+}
---
+++ src/DataHandling.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Core;
+
+/**
+ * Manage Get, Post, Cookies, Files, Server Data, Session Data and Env data.  
+ */
+class DataHandling
+{
+    public $get;
+    public $post;
+    public $cookie;
+    public $request;
+    public $files;
+    public $server;
+    public $session;
+    public $env;
+
+    public function __construct()
+    {
+        $this->get = (object) $_GET;
+        $this->post = (object) $_POST;
+        $this->cookie = (object) $_COOKIE;
+        $this->request = (object) $_REQUEST;
+        $this->files = (object) $_FILES;
+
+        if (isset($_SESSION)) {
+            $this->session = (object) $_SESSION;
+        } else {
+            $this->session = null;
+        }
+
+        # Looing for .env at the root directory
+        $env = parse_ini_file(__DIR__ . '/../.env');
+        $this->env = (object) $env;
+    }
+}
